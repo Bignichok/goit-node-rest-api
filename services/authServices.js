fe7bbdbb4e83c6d01a3ac8ffaeedba575dff5c76
bignichok <inikita546@gmail.com>
2025-06-18 17:18:37 +0300

    05-avatars and tests

--- services/authServices.js
+++ services/authServices.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import gravatar from "gravatar";
 
 import HttpError from "../helpers/HttpError.js";
 
@@ -10,7 +11,15 @@ export const registerUser = async ({ email, password }) => {
     throw HttpError(409, "Email is already in use");
   }
 
-  const newUser = await User.create({ email, password });
+  // Generate gravatar URL
+  const avatarUrl = gravatar.url(email, {
+    s: "200", // Size 200x200
+    r: "pg", // Rating: G, PG, R, X
+    d: "identicon", // Default image type if no gravatar found
+    protocol: "https", // Use HTTPS
+  });
+
+  const newUser = await User.create({ email, password, avatarUrl });
   return newUser;
 };
 
@@ -32,12 +41,12 @@ export const loginUser = async (email, password) => {
 
   // Store token in user record
   await user.update({ token });
-
   return {
     token,
     user: {
       email: user.email,
       subscription: user.subscription,
+      avatarUrl: user.avatarUrl,
     },
   };
 };
@@ -60,6 +69,7 @@ export const getCurrentUser = async (userId) => {
   return {
     email: user.email,
     subscription: user.subscription,
+    avatarUrl: user.avatarUrl,
   };
 };
 
@@ -80,5 +90,20 @@ export const updateUserSubscription = async (userId, subscription) => {
   return {
     email: user.email,
     subscription: user.subscription,
+  };
+};
+
+export const updateUserAvatar = async (userId, avatarUrl) => {
+  const user = await User.findByPk(userId);
+  if (!user) {
+    throw HttpError(404, "User not found");
+  }
+
+  await user.update({ avatarUrl });
+
+  return {
+    email: user.email,
+    subscription: user.subscription,
+    avatarUrl: user.avatarUrl,
   };
 };
